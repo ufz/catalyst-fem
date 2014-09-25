75fe29ecd339c1ec328a5e5886b4c27c693a1438
Lars Bilke <lars.bilke@ufz.de>
2014-09-25 16:26:26 +0200

    Fem edition, initial.
    Paraview: https://github.com/Kitware/ParaView/commit/069722 (tag 4.2)
    Edition:  https://github.com/bilke/ogs-catalyst-editions/commit/e946d9

---
+++ VTK/IO/XML/fem.catalyst.cmake
@@ -0,0 +1,12 @@
+list(APPEND Module_SRCS
+  vtkXMLCompositeDataWriter.cxx
+  vtkXMLImageDataWriter.cxx
+  vtkXMLMultiBlockDataWriter.cxx
+  vtkXMLPolyDataWriter.cxx
+  vtkXMLRectilinearGridWriter.cxx
+  vtkXMLStructuredDataWriter.cxx
+  vtkXMLStructuredGridWriter.cxx
+  vtkXMLUnstructuredDataWriter.cxx
+  vtkXMLUnstructuredGridWriter.cxx
+  vtkXMLWriter.cxx
+  )
